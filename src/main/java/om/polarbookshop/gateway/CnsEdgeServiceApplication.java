b9f6ca5b02fb7344bb4b6bb8ff9cb39051f2e825
Howard Egana <howard.cu@gmail.com>
2023-07-27 09:34:40 -0400

    ch9 - Initial commit

---
+++ src/main/java/om/polarbookshop/gateway/CnsEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package om.polarbookshop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsEdgeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/om/polarbookshop/gateway/CnsEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package om.polarbookshop.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsEdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
